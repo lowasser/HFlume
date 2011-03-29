3e47fd54079ca717d6171c721fe6422bb7c8dd6b
Louis Wasserman <wasserman.louis@gmail.com>
2011-03-28 22:57:24 -0500

    Added ArrowChoice instance for MRParallel

--- Control/Arrow/MapReduce/Parallel.hs
+++ Control/Arrow/MapReduce/Parallel.hs
@@ -18,34 +18,37 @@ import Control.Concurrent.Chan.Endable
 
 import GHC.Conc
 
+import Prelude hiding ((.))
+
 newtype MRParallel input output = MRParallel (forall x . 
   MRInput (x, input)
   -> MROutput (x, output)
-  -> IO ())
+  -> IO (IO ())) -- returns a "wait till done" command
 
 instance Category MRParallel where
-  id = MRParallel $ \ input output -> mapInputM_ (emit output) input
+  id = MRParallel $ \ input output -> do
+    flag <- newEmptyMVar
+    forkIO $ do
+      mapInputM_ (emit output) input
+      putMVar flag ()
+    return (takeMVar flag)
   MRParallel f . MRParallel g = MRParallel $ \ input output -> do
     (gOut, fIn) <- newPipe
-    forkIO (g input gOut)
-    f fIn output
+    gDone <- g input gOut
+    fDone <- f fIn output
+    return (gDone >> fDone)
 
 wrap :: (a -> b) -> (c -> d) -> MRParallel b c -> MRParallel a d
 wrap f g (MRParallel run) = MRParallel $ \ input output ->
   run (fmap (fmap f) input) (cofmap (fmap g) output)
 
 instance Arrow MRParallel where
-  arr f = MRParallel $ \ input output -> void $ do
-    let nThreads = numCapabilities
-    test <- newEmptyMVar
-    replicateM_ nThreads $ forkIO $ do
-      mapInputM_ (\ (x, a) -> do
-	b <- evaluate (f a)
-	emit output (x, b)) input
-      putMVar test () -- this thread is done
+  arr f = MRParallel $ \ input output -> do -- not strict
+    flag <- newEmptyMVar
     forkIO $ do
-      replicateM_ nThreads (takeMVar test) -- wait for everyone to finish
-      reportEnd output
+      mapInputM_ (emit output . fmap f) input
+      putMVar flag ()
+    return (takeMVar flag)
   first (MRParallel run) = MRParallel $ \ input output ->
     let runInput = fmap (\ (x, (a, b)) -> ((x, b), a)) input
 	runOutput = cofmap (\ ((x, b), a) -> (x, (a, b))) output
@@ -54,3 +57,53 @@ instance Arrow MRParallel where
     let runInput = fmap (\ (x, (a, b)) -> ((x, a), b)) input
 	runOutput = cofmap (\ ((x, a), b) -> (x, (a, b))) output
     in run runInput runOutput
+
+instance ArrowChoice MRParallel where
+  left (MRParallel run) = MRParallel $ \ input output -> do
+    inSem <- newEmptyMVar
+    (leftOut, runInput) <- newPipe
+    let runOutput = cofmap (fmap Left) output
+    forkIO $ do
+      mapInputM_ (\ (x, i) -> case i of
+	Left a	-> emit leftOut (x, a)
+	Right b	-> emit output (x, Right b)) input
+      putMVar inSem ()
+    leftTerm <- run runInput runOutput
+    return (takeMVar inSem >> leftTerm)
+  right (MRParallel run) = MRParallel $ \ input output -> do
+    inSem <- newEmptyMVar
+    (rightOut, runInput) <- newPipe
+    let runOutput = cofmap (fmap Right) output
+    forkIO $ do
+      mapInputM_ (\ (x, i) -> case i of
+	Right a	-> emit rightOut (x, a)
+	Left b	-> emit output (x, Left b)) input
+      putMVar inSem ()
+    rightTerm <- run runInput runOutput
+    return (takeMVar inSem >> rightTerm)
+  MRParallel runLeft +++ MRParallel runRight = MRParallel $ \ input output -> do
+    inSem <- newEmptyMVar
+    (leftPipe, leftIn) <- newPipe
+    (rightPipe, rightIn) <- newPipe
+    let leftOut = cofmap (fmap Left) output
+	rightOut = cofmap (fmap Right) output
+    forkIO $ do
+      mapInputM_ (\ (x, i) -> case i of
+	Left a	-> emit leftPipe (x, a)
+	Right b	-> emit rightPipe (x, b)) input
+      putMVar inSem ()
+    leftTerm <- runLeft leftIn leftOut
+    rightTerm <- runRight rightIn rightOut
+    return (takeMVar inSem >> leftTerm >> rightTerm)
+  MRParallel runLeft ||| MRParallel runRight = MRParallel $ \ input output -> do
+    inSem <- newEmptyMVar
+    (leftPipe, leftIn) <- newPipe
+    (rightPipe, rightIn) <- newPipe
+    forkIO $ do
+      mapInputM_ (\ (x, i) -> case i of
+	Left a	-> emit leftPipe (x, a)
+	Right b	-> emit rightPipe (x, b)) input
+      putMVar inSem ()
+    leftTerm <- runLeft leftIn output
+    rightTerm <- runRight rightIn output
+    return (takeMVar inSem >> leftTerm >> rightTerm)
