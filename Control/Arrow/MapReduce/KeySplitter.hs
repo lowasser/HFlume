edb9bec9f332849593b15223d7271e692a9d2cfd
Louis Wasserman <wasserman.louis@gmail.com>
2011-03-29 15:18:53 -0500

    Added KeySplitter output modifier

---
+++ Control/Arrow/MapReduce/KeySplitter.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE GADTs #-}
+module Control.Arrow.MapReduce.KeySplitter where
+
+import Control.Output.Class
+import Control.Monad
+import Control.Concurrent.MVar
+
+data KeySplitter f a where
+  KeySplitter :: Eq k => !(MVar [(k, f (x, a))]) -> (k -> IO (f (x, a))) -> KeySplitter f (k, x, a)
+
+newKeySplitter :: Eq k => (k -> IO (f (x, a))) -> IO (KeySplitter f a)
+newKeySplitter newKeyMaker = do
+  outsVar <- newMVar []
+  return (KeySplitter outsVar newKeyMaker)
+
+instance Output f => Output (KeySplitter f) where
+  emit (KeySplitter outsVar newKey) (k, x, a) = modifyMVar_ outsVar $ let
+    emitter outs0@((k', kOut):_)
+      | k == k'	= emit kOut (x, a) >> return outs0
+    emitter (kOut:outs)
+      = liftM (kOut:) (emitter outs)
+    emitter [] = do
+      kOut <- newKey k
+      emit kOut (x, a)
+      return [(k, kOut)]
+    in emitter
+  reportEnd (KeySplitter outsVar _) = takeMVar outsVar >>= \ outs -> sequence_ [reportEnd kOut | (_, kOut) <- outs]
+   -- we don't replace it
