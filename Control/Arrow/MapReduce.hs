f67307e32f6a1389636f9d67239f74e6a124101b
Louis Wasserman <wasserman.louis@gmail.com>
2011-03-28 21:28:21 -0500

    Add combine to Control.Arrow.MapReduce

--- Control/Arrow/MapReduce.hs
+++ Control/Arrow/MapReduce.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE TupleSections #-}
-module Control.Arrow.MapReduce where
+module Control.Arrow.MapReduce (ArrowMapReduce(..), mapCombine, mapMany, combine) where
 
 import Data.Hashable
 import Control.DeepSeq
@@ -17,22 +17,35 @@ class Arrow a => ArrowMapReduce a where
   mapManyReduce :: (Hashable k, NFData c, NFData d) => 
     (MRInput b -> MROutput (k, c) -> IO ()) -> (k -> MRInput c -> MROutput d -> IO ()) -> a b d
 
+{-# INLINE mapFold1 #-}
+mapFold1 :: (a -> b) -> (b -> b -> b) -> MRInput a -> IO (Maybe b)
+mapFold1 f (*) inp = do
+  a1 <- tryGet inp
+  case a1 of
+    Nothing	-> return Nothing
+    Just a1	-> do	bb <- foldInput (\ b a -> b * f a) (f a1) inp
+			return (Just bb)
+
+{-# INLINE emitMaybe #-}
+emitMaybe :: MROutput a -> Maybe a -> IO ()
+emitMaybe out = maybe (return ()) (emit out)
+
 {-# INLINE mapCombine #-}
 mapCombine :: (ArrowMapReduce a, NFData c) => (b -> c) -> (c -> c -> c) -> a b c
-mapCombine mp (*) = mapManyReduce
+mapCombine mp (*) = mapManyReduce 
   (\ inB outKC -> do
-    b1 <- tryGet inB
-    case b1 of
-      Nothing	-> return ()
-      Just b1	-> do	cc <- foldInput' (\ c b -> c * mp b) (mp b1) inB
-			emit outKC ((), cc))
-  (\ _ inC outD -> do
-    c1 <- tryGet inC
-    case c1 of
-      Nothing	-> return ()
-      Just c1	-> foldInput' (*) c1 inC >>= emit outD)
+    cc <- mapFold1 mp (*) inB
+    emitMaybe outKC $ fmap ((),) cc)
+  (\ _ inC outD -> mapFold1 id (*) inC >>= emitMaybe outD)
 
 mapMany :: (ArrowMapReduce a, NFData c) => (b -> [c]) -> a b c
 mapMany k = mapManyReduce
   (\ inB outKC -> mapInputM_ (\ b -> mapM_ (emit outKC . ((),)) (k b)) inB)
   (\ _ inC outD -> mapInputM_ (emit outD) inC)
+
+combine :: (ArrowMapReduce a, NFData b) => (b -> b -> b) -> a b b
+combine (*) = mapManyReduce
+  (\ inB outKC -> do
+      bb <- mapFold1 id (*) inB
+      emitMaybe outKC $ fmap ((),) bb)
+  (\ _ inC outD -> mapFold1 id (*) inC >>= emitMaybe outD)
