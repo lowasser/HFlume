7a2bfaf3db07da2af6685e2c1cc1b6ab6bbfb1b8
Louis Wasserman <wasserman.louis@gmail.com>
2011-04-08 13:48:18 -0500

    Unique monad transformer

---
+++ Control/Parallel/Flume/Unique.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE MagicHash, UnboxedTuples #-}
+{-# OPTIONS -funbox-strict-fields #-}
+module Control.Parallel.Flume.Unique (UniqueId, UniqueT, execUniqueT, newUnique) where
+
+import Control.Monad
+
+import GHC.Exts
+
+newtype UniqueId = Unique Int deriving (Eq, Ord)
+
+data StrU a = StrU Int# a
+newtype UniqueT m a = UniqueT {runUniqueT :: Int# -> m (StrU a)}
+
+execUniqueT :: Monad m => UniqueT m a -> m a
+execUniqueT m = do
+  StrU _ a <- runUniqueT m 0#
+  return a
+
+instance Monad m => Monad (UniqueT m) where
+  {-# INLINE return #-}
+  return a = UniqueT $ \ i# -> return (StrU i# a)
+  {-# INLINE (>>=) #-}
+  m >>= k = UniqueT $ \ i# -> do
+    StrU i'# a <- runUniqueT m i#
+    runUniqueT (k a) i'#
+
+newUnique :: Monad m => UniqueT m UniqueId
+newUnique = UniqueT $ \ i# -> return $ StrU (i# +# 1#) (Unique (I# i#))
