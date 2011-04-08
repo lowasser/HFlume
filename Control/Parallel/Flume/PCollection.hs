adf8475ae6ab3b2368c76710b7266f4405156a77
Louis Wasserman <wasserman.louis@gmail.com>
2011-04-08 17:10:54 -0500

    Move methods to PTable as appropriate

--- Control/Parallel/Flume/PCollection.hs
+++ Control/Parallel/Flume/PCollection.hs
@@ -4,17 +4,9 @@ module Control.Parallel.Flume.PCollection (
   fromVector,
   flatten,
   map,
-  mapValues,
-  mapValuesWithKey,
   filter,
-  filterValues,
-  filterValuesWithKey,
   mapMaybe,
-  mapMaybeValues,
-  mapMaybeValuesWithKey,
-  mapMany,
-  mapManyValues,
-  mapManyValuesWithKey) where
+  mapMany) where
 
 import Control.Parallel.Flume.Types
 import Control.Parallel.Flume.Unique
@@ -49,35 +41,11 @@ mkDoFn mkFn coll = do
 map :: (a -> b) -> PCollection s a -> Flume s (PCollection s b)
 map f = mkDoFn (`Map` f)
 
-mapValues :: (a -> b) -> PTable s k a -> Flume s (PTable s k b)
-mapValues f = mapValuesWithKey (const f)
-
-mapValuesWithKey :: (k -> a -> b) -> PTable s k a -> Flume s (PTable s k b)
-mapValuesWithKey f = mkDoFn (`MapValues` f)
-
 filter :: (a -> Bool) -> PCollection s a -> Flume s (PCollection s a)
 filter p = mapMaybe (\ a -> if p a then Just a else Nothing)
-
-filterValues :: (a -> Bool) -> PTable s k a -> Flume s (PTable s k a)
-filterValues p = filterValuesWithKey (const p)
-
-filterValuesWithKey :: (k -> a -> Bool) -> PTable s k a -> Flume s (PTable s k a)
-filterValuesWithKey p = mapMaybeValuesWithKey (\ k a -> if p k a then Just a else Nothing)
 
 mapMaybe :: (a -> Maybe b) -> PCollection s a -> Flume s (PCollection s b)
 mapMaybe f = mkDoFn (`MapMaybe` f)
 
-mapMaybeValuesWithKey :: (k -> a -> Maybe b) -> PTable s k a -> Flume s (PTable s k b)
-mapMaybeValuesWithKey f = mkDoFn (`MapMaybeValues` f)
-
-mapMaybeValues :: (a -> Maybe b) -> PTable s k a -> Flume s (PTable s k b)
-mapMaybeValues f = mapMaybeValuesWithKey (const f)
-
 mapMany :: (a -> [b]) -> PCollection s a -> Flume s (PCollection s b)
 mapMany f = mkDoFn (`ConcatMap` f)
-
-mapManyValuesWithKey :: (k -> a -> [b]) -> PTable s k a -> Flume s (PTable s k b)
-mapManyValuesWithKey f = mkDoFn (`ConcatMapValues` f)
-
-mapManyValues :: (a -> [b]) -> PTable s k a -> Flume s (PTable s k b)
-mapManyValues f = mapManyValuesWithKey (const f)
--- Control/Parallel/Flume/PTable.hs
+++ Control/Parallel/Flume/PTable.hs
@@ -2,12 +2,23 @@ module Control.Parallel.Flume.PTable where
 
 import Control.Parallel.Flume.Types
 import Control.Parallel.Flume.PCollection
+import Control.Parallel.Flume.Unique
 
 import Control.Monad
 
 import Data.Hashable
 
 import Prelude hiding (map)
+
+parallelDo :: PDo s a b -> PCollection s a -> Flume s (PCollection s b)
+parallelDo doFn coll = do
+  collId <- newID
+  return (Parallel collId doFn coll)
+
+mkDoFn :: (UniqueId -> PDo s a b) -> PCollection s a -> Flume s (PCollection s b)
+mkDoFn mkFn coll = do
+  doFnId <- newID
+  parallelDo (mkFn doFnId) coll
 
 count :: (Eq a, Hashable a) => PCollection s a -> Flume s (PTable s a Int)
 count = map (\ a -> (a, 1)) >=> groupByKeyOneShot >=> combineValues (+)
@@ -29,3 +40,27 @@ combineValuesWithKey comb coll = do
 
 combineValues :: (Eq k, Hashable k) => (a -> a -> a) -> PTable s k (OneShot s a) -> Flume s (PTable s k a)
 combineValues (*) = combineValuesWithKey (const (*))
+
+mapValues :: (a -> b) -> PTable s k a -> Flume s (PTable s k b)
+mapValues f = mapValuesWithKey (const f)
+
+mapValuesWithKey :: (k -> a -> b) -> PTable s k a -> Flume s (PTable s k b)
+mapValuesWithKey f = mkDoFn (`MapValues` f)
+
+filterValues :: (a -> Bool) -> PTable s k a -> Flume s (PTable s k a)
+filterValues p = filterValuesWithKey (const p)
+
+filterValuesWithKey :: (k -> a -> Bool) -> PTable s k a -> Flume s (PTable s k a)
+filterValuesWithKey p = mapMaybeValuesWithKey (\ k a -> if p k a then Just a else Nothing)
+
+mapMaybeValuesWithKey :: (k -> a -> Maybe b) -> PTable s k a -> Flume s (PTable s k b)
+mapMaybeValuesWithKey f = mkDoFn (`MapMaybeValues` f)
+
+mapMaybeValues :: (a -> Maybe b) -> PTable s k a -> Flume s (PTable s k b)
+mapMaybeValues f = mapMaybeValuesWithKey (const f)
+
+mapManyValues :: (a -> [b]) -> PTable s k a -> Flume s (PTable s k b)
+mapManyValues f = mapManyValuesWithKey (const f)
+
+mapManyValuesWithKey :: (k -> a -> [b]) -> PTable s k a -> Flume s (PTable s k b)
+mapManyValuesWithKey f = mkDoFn (`ConcatMapValues` f)
