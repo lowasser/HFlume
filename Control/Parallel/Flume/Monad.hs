b8e2a8c25c3fde4b71ba25fc36264cbaef329746
Louis Wasserman <wasserman.louis@gmail.com>
2011-04-08 14:00:26 -0500

    Added unique identifiers to PCollections, etc.

--- Control/Parallel/Flume/Monad.hs
+++ Control/Parallel/Flume/Monad.hs
@@ -4,6 +4,7 @@ module Control.Parallel.Flume.Monad where
 import Control.Parallel.Flume.Types
 
 import Control.Applicative
+import Control.Monad
 
 import Data.Monoid
 import Data.Vector (toList)
@@ -26,22 +27,24 @@ execFlumeSequential :: (forall s . Flume s (PObject s a)) -> a
 execFlumeSequential m = runFlume (seqExecObj (runFlume m))
 
 seqExecObj :: PObject s a -> Flume s a
-seqExecObj (Literal a) = return a
-seqExecObj (Sequential c) = seqExecColl c
-seqExecObj (MapOb f ob) = fmap f (seqExecObj ob)
-seqExecObj (Operate f x) = seqExecObj f <*> seqExecObj x
-seqExecObj (Concat c) = fmap mconcat (seqExecColl c)
+seqExecObj (Literal _ a) = return a
+seqExecObj (Sequential _ c) = seqExecColl c
+seqExecObj (MapOb _ f ob) = fmap f (seqExecObj ob)
+seqExecObj (Operate _ f x) = seqExecObj f <*> seqExecObj x
+seqExecObj (Concat _ c) = fmap mconcat (seqExecColl c)
 
 seqExecColl :: PCollection s a -> Flume s [a]
-seqExecColl (Parallel pdo c) =
+seqExecColl (Parallel _ pdo c) =
   seqExecPDo pdo (runFlume $ seqExecColl c)
-seqExecColl (Explicit xs) = return (toList xs)
-seqExecColl (GroupByKeyOneShot ungrouped) = do
+seqExecColl (Explicit _ xs) = return (toList xs)
+seqExecColl (GroupByKeyOneShot _ ungrouped) = do
   xs <- seqExecColl ungrouped
   let m = foldr (\ (k, a) -> HM.insertWith (\ _ old -> a:old) k [a]) HM.empty xs
   return (HM.toList $ fmap toOneShot m)
 
 seqExecPDo :: PDo s a b -> [a] -> Flume s [b]
-seqExecPDo (Map f) xs = return $ fmap f xs
-seqExecPDo (MapMaybe f) xs = return $ [y | x <- xs, Just y <- return (f x)]
-seqExecPDo (ConcatMap f) xs = return $ concatMap f xs
+seqExecPDo Identity xs = return xs
+seqExecPDo (Map _ f) xs = return $ fmap f xs
+seqExecPDo (MapMaybe _ f) xs = return $ [y | x <- xs, Just y <- return (f x)]
+seqExecPDo (ConcatMap _ f) xs = return $ concatMap f xs
+seqExecPDo (f :<<: g) xs = (seqExecPDo g >=> seqExecPDo f) xs
--- Control/Parallel/Flume/Types.hs
+++ Control/Parallel/Flume/Types.hs
@@ -1,10 +1,9 @@
 {-# LANGUAGE GADTs, EmptyDataDecls, TupleSections, Rank2Types #-}
 module Control.Parallel.Flume.Types where
 
+import Control.Parallel.Flume.Unique
+
 import Control.Category
-import Control.Applicative
-import Control.Arrow
-import Control.Monad
 import Data.Vector
 import Data.Maybe
 import Data.Monoid
@@ -13,9 +12,9 @@ import qualified Data.List as L
 import Prelude hiding ((.), id)
 
 data PCollection s a where
-  Explicit :: Vector a -> PCollection s a
-  Parallel :: PDo s a b -> PCollection s a -> PCollection s b
-  GroupByKeyOneShot :: (Eq k, Hashable k) => PCollection s (k, a) -> PCollection s (k, OneShot s a)
+  Explicit :: !UniqueId -> Vector a -> PCollection s a
+  Parallel :: !UniqueId -> PDo s a b -> PCollection s a -> PCollection s b
+  GroupByKeyOneShot :: (Eq k, Hashable k) => !UniqueId -> PCollection s (k, a) -> PCollection s (k, OneShot s a)
 
 data OneShot s a = OneShot {runOneShot :: forall b . (b -> a -> b) -> b -> b}
 
@@ -23,58 +22,23 @@ toOneShot :: [a] -> OneShot s a
 toOneShot xs = OneShot (\ f z -> L.foldl f z xs)
 
 data PDo s a b where
-  Map :: (a -> b) -> PDo s a b
-  MapMaybe :: (a -> Maybe b) -> PDo s a b
-  ConcatMap :: (a -> [b]) -> PDo s a b
+  Identity :: PDo s a a
+  Map :: !UniqueId -> (a -> b) -> PDo s a b
+  MapMaybe :: !UniqueId -> (a -> Maybe b) -> PDo s a b
+  ConcatMap :: !UniqueId -> (a -> [b]) -> PDo s a b
+  (:<<:) :: PDo s b c -> PDo s a b -> PDo s a c {- the right operation is never a sequence -}
   -- TODO: side inputs
 
 instance Category (PDo s) where
-  id = Map id
-  Map f . Map g = Map (f . g)
-  Map f . MapMaybe g = MapMaybe (fmap f . g)
-  Map f . ConcatMap g = ConcatMap (fmap f . g)
-  MapMaybe f . Map g = MapMaybe (f . g)
-  MapMaybe f . MapMaybe g = MapMaybe (f <=< g)
-  MapMaybe f . ConcatMap g = ConcatMap (\ a -> [c | b <- g a, Just c <- return (f b)])
-  ConcatMap f . Map g = ConcatMap (f . g)
-  ConcatMap f . MapMaybe g = ConcatMap (f <=< maybeToList . g)
-  ConcatMap f . ConcatMap g = ConcatMap (f <=< g)
-
-instance Arrow (PDo s) where
-  arr = Map
-  first (Map f) = Map (first f)
-  first (MapMaybe f) = MapMaybe (runKleisli $ first $ Kleisli f)
-  first (ConcatMap f) = ConcatMap (runKleisli $ first $ Kleisli f)
-  second (Map f) = Map (second f)
-  second (MapMaybe f) = MapMaybe (runKleisli $ second $ Kleisli f)
-  second (ConcatMap f) = ConcatMap (runKleisli $ second $ Kleisli f)
-  Map f *** Map g = Map (f *** g)
-  Map f *** MapMaybe g = MapMaybe (\ (a, b) -> fmap (f a,) (g b))
-  Map f *** ConcatMap g = ConcatMap (\ (a, b) -> fmap (f a,) (g b))
-  MapMaybe f *** Map g = MapMaybe (\ (a, b) -> fmap (,g b) (f a))
-  MapMaybe f *** MapMaybe g = MapMaybe (runKleisli $ Kleisli f *** Kleisli g)
-  MapMaybe f *** ConcatMap g = ConcatMap (\ (a, b) -> case f a of
-    Nothing	-> []
-    Just a'	-> fmap (a',) (g b))
-  ConcatMap f *** Map g = ConcatMap (\ (a, b) -> fmap (,g b) (f a))
-  ConcatMap f *** MapMaybe g = ConcatMap (\ (a, b) -> case g b of
-    Nothing	-> []
-    Just b'	-> fmap (,b') (f a))
-  ConcatMap f *** ConcatMap g = ConcatMap (runKleisli $ Kleisli f *** Kleisli g)
+  id = Identity
+  Identity . g = g
+  f . Identity = f
+  f . (g :<<: h) = (f . g) :<<: h
+  f . g = f :<<: g
 
 data PObject s a where
-  Operate :: PObject s (a -> b) -> PObject s a -> PObject s b
-  MapOb :: (a -> b) -> PObject s a -> PObject s b
-  Sequential :: PCollection s a -> PObject s [a]
-  Concat :: Monoid a => PCollection s a -> PObject s a
-  Literal :: a -> PObject s a
-
-instance Functor (PObject s) where
-  fmap f (MapOb g ob) = MapOb (f . g) ob
-  fmap f (Literal a) = Literal (f a)
-  fmap f ob = MapOb f ob
-
-instance Applicative (PObject s) where
-  pure = Literal
-  Literal f <*> ob = fmap f ob
-  f <*> ob = Operate f ob
+  Operate :: !UniqueId -> PObject s (a -> b) -> PObject s a -> PObject s b
+  MapOb :: !UniqueId -> (a -> b) -> PObject s a -> PObject s b
+  Sequential :: !UniqueId -> PCollection s a -> PObject s [a]
+  Concat :: Monoid a => !UniqueId -> PCollection s a -> PObject s a
+  Literal :: !UniqueId -> a -> PObject s a
